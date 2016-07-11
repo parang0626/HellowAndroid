e133c5adc605d83a07303d8550e60511d594d6ea
parang0626 <blue0626@naver.com>
2016-07-11 11:17:43 +0900

    Initial commit

---
+++ app/src/main/java/kr/co/openit/giparang/hellowandroid/MainActivity.java
@@ -0,0 +1,17 @@
+package kr.co.openit.giparang.hellowandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        for(int i=0;i<10;i++)
+        {
+            int j = i;
+        }
+    }
+}
